215658b9bc7a3ecd26b870a65ce71a261f6cb635
Clecio-Gabriel <cleciogabofelix@gmail.com>
2026-04-14 00:11:27 -0300

    brick_sort done, more tests needed

---
+++ headers/sort.hpp
@@ -0,0 +1,38 @@
+#include <iterator>
+#include <algorithm>
+
+template <typename Itr>
+void brick_sort(Itr first, Itr last){
+    bool is_sorted = false; //sort with flag
+
+    while (not is_sorted){
+        bool troca = false;
+
+        //EVEN ITERATIONS
+        auto left{first};
+        while (std::distance(left, last)>1){
+            auto right{left+1};
+            if (*left>*right){
+                std::iter_swap(left, right);
+                troca = true;
+            }
+            left+=2;
+        }
+
+        //ODD ITERATIONS
+        left = first+1;
+        while (std::distance(left, last)>1){
+            auto right{left+1};
+            if (*left>*right){
+                std::iter_swap(left, right);
+                troca = true;
+            }
+            left+=2;
+        }
+
+        if (not troca){
+            is_sorted = true;
+        }
+    }
+    
+}
